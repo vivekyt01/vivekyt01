b38faf49cb64fe4edaf30f5bde01f52c0613ce91
Vivekyt01 <vivekchimankar83@gmail.com>
2024-06-14 13:42:58 +0530

    add rafce

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -3,3 +3,12 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import React from 'react'
+
+const setupTests = () => {
+  return (
+    <div>setupTests</div>
+  )
+}
+
+export default setupTests
